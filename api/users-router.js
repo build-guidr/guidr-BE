63741d704d51bcefc221c39e188691c752b512a2
Ian Be <ian84be@gmail.com>
2019-04-16 14:01:50 -0500

    refactor users-router

--- api/users-router.js
+++ api/users-router.js
@@ -2,23 +2,27 @@ const router = require('express').Router();
 
 const Users = require('./users-model.js');
 
-router.get('/', (req, res) => {
-	Users.find()
-		.then(users => {
-			res.json(users);
-		})
-		.catch(err => res.send(err));
+// restricted route /api/users
+router.get('/', async (req, res) => {
+	try {
+		const allUsers = await Users.find();
+		res.status(200).json(allUsers);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
 	try {
 		const removed = await Users.remove(req.params.id);
 		if (removed) {
-			res.status(204).json({success: 'User removed'}); 
+			res.status(204).json({ success: 'User removed' });
 		} else {
-			res.status(404).json({ message: "The User with the specified ID does not exist." });    
+			res
+				.status(404)
+				.json({ message: 'The User with the specified ID does not exist.' });
 		}
-	} catch(err) {
+	} catch (err) {
 		res.status(500).json(err);
 	}
 });
