1da4561019f12c989cd54aee115fa87b99db5685
Ian Be <ian84be@gmail.com>
2019-04-17 16:58:59 -0500

    add faker, create fakeUsers in seed file

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,3 +1,4 @@
+
 exports.seed = function(knex, Promise) {
 	// Deletes ALL existing entries
 	return knex('users')
@@ -5,7 +6,19 @@ exports.seed = function(knex, Promise) {
 		.then(function() {
 			// Inserts seed entries
 			return knex('users').insert([
-				{ id: 1, username: 'julian', password: 'pass' }
+				...fakeUsers,
+				{id:1,username:'admin',password:'admin'}
 			]);
 		});
 };
+
+const faker = require('faker');
+const fakeUsers = [];
+
+for (let i = 2; i > 12; i++) {
+    let id = i;
+    let username = faker.internet.userName();
+    let password = faker.internet.password();
+    let fakeUser = {id,username,password};
+    fakeUsers.push(fakeUser);
+}
